ff76222ff4c44d215e527e24376064aaa6cc8d1b
Edgar Corona <coronam.edgar@gmail.com>
2025-10-09 23:25:40 -0600

    smpt

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -6,6 +6,9 @@ import { User } from "@prisma/client";
    📧 Configuración de transporte con Gmail
 ----------------------------------------------------------- */
 const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,           // 465 = SSL, 587 = STARTTLS
+  secure: true,        // true para 465, false para 587
   service: "gmail",
   auth: {
     user: ENV.MAIL_CLIENT,
